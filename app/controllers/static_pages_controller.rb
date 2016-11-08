2ba0095fae83321bcc095738d1c21f7d4042a3a5
KeiTamiya <webcreatorhouse@gmail.com>
2016-11-08 01:39:38 +0000

    [add] top selectable ranking

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,9 +6,12 @@ class StaticPagesController < ApplicationController
     @age_shop_id = Age.group(:shop_id).order('count_shop_id desc').limit(10).count(:shop_id).keys
     @age_shops = Shop.find(@age_shop_id).sort_by{|i| @age_shop_id.index(i.id)}
     
-    
     @want_shop_id = Want.group(:shop_id).order('count_shop_id desc').limit(10).count(:shop_id).keys
     @want_shops = Shop.find(@want_shop_id).sort_by{|i| @want_shop_id.index(i.id)}
+    
+    @initialTopShop = @age_shops[0]
+    @age_rank3 = @age_shops[0..2]
+    @want_rank3 = @want_shops[4..6]
   end
   
   def introduce
